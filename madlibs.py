ccc9d500cf715a2bd246d65a66ac00a38a98ea19
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-02 12:35:23 -0700

    updated madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,14 +1,30 @@
-#Mad libs lab
+#file saved as madlibs2.property
+#import random module
+import random
+print("*" * 10)
 
-print("You've been hired to make answering machines for the lazy nastolgic public. Please input the following prompts for our customer")
-lastname = input("What is our client's last name? ")
-event = input("why can't the client answer the phone? ")
-place = input("Where is the client at? ")
-animal = input("State the clinets favorite animal ")
-name = input("state another name")
+#create a infinite while loop to repeat code until the conditions are broken.
+while True:
+    print("Welcome to Shopping List Madlibs!")
+    print("You are going shopping and are preparing a list. Enter the items you need from each area of the store, seperated by commas")
 
-"""
-madlib:
-Hello you've reached the [last name]'s residence. WE cannot come to the phone today because [event] and we had to flee to [place]. If you need us or our pet [animal], [name], please email us at [shameful secret]@gmail.com. Thanks!
+    #get user input. This
+    aisleitems = input("What items are needed from the aisle? ")
+    meatitems = input("What items are needed from the meat and dairy department? ")
+    frozenitems = input("What frozen items are needed? ")
 
-"""
+    #split user input at ever comma, store split items in a list
+    aisle = aisleitems.split(",")
+    meat = meatitems.split(",")
+    frozen = frozenitems.split(",")
+
+    #printf the madlibs statement with random choices from each list above.
+    print(f"While going shopping you found" + random.choice(aisle) + " and " + random.choice(meat) + ", but they were out of " + random.choice(frozen))
+    
+    again = input("Would you like to try again? Type 'y' for yes or 'n' for no. ")
+    #break the while loop if user input equals anything but "y"
+    if again != "y":
+        break
+
+print("Thank you for using Shopping List Madlibs!")
+print("*" * 10)
