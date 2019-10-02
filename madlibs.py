cdb48e361816369482d91c3d5533d1ea5a739ffe
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-01 20:55:00 -0700

    added code to madlibs

---
+++ madlibs.py
@@ -0,0 +1,5 @@
+#Mad libs lab
+
+print("Welcome to python's madlibs program. Enter your answers to the following prompts and hit the 'return' key. When finished, your madlibs will be displayed")
+adj1 = input("Type an adjective: ")
+print(adj1)
