a18aafab9489a93dea74cd7f46ff8369ae70412b
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-04 21:20:31 -0700

    finished RPS

--- RPS.py
+++ RPS.py
@@ -1,47 +1,44 @@
 import random
 
-def get_variables():
-    userRPC = input("Type your choice, 'rock' 'paper', or 'scissors': ")
-    choices = ['rock', 'paper', 'scissors']
-    botRPS = random.choice(choices)
+
+userRPC = input("Type your choice, 'rock' 'paper', or 'scissors': ")
+choices = ['rock', 'paper', 'scissors']
+botRPS = random.choice(choices)
+
 def userRock():
-    if userRPC != 'rock'
-        break
-    elif:
-        if botRPS == 'rock'
+    if userRPC != 'rock':
+        return
+    elif botRPS == 'rock':
         print("You both choose rock, it's a tie!")
-    elif:
-        if botRPS == 'paper'
+    elif botRPS == 'paper':
         print("The computer used paper. Paper cover rock, you loose!")
-    else:
+    elif botRPS == 'Scissors':
         print("The computer used scissors. Rock breaks scissors, you win!")
+    return
 def userPaper():
-    if userRPC != 'paper'
-        break
-    elif:
-        if botRPS == 'rock'
+    if userRPC != 'paper':
+        return
+    elif botRPS == 'rock':
         print("The computer used rock. Paper covers rock, you win!")
-    elif:
-        if botRPS == 'paper'
+    elif botRPS == 'paper':
         print("You both choose paper, it's a tie")
-    else:
+    elif botRPS == 'Scissors':
         print("The computer used scissors. Scissors cut paper, you loose!")
+    return
 def userScissors():
-    if userRPC != 'scissors'
-        break
-    elif:
-        if botRPS == 'rock'
+    if userRPC != 'paper':
+        return
+    elif botRPS == 'rock':
         print("The computer used rock. Rock breaks scissors, you loose!")
-    elif:
-        if botRPS == 'paper'
+    elif botRPS == 'paper':
         print("The computer used paper. Scissors cut paper, you win!")
-    else:
+    elif botRPS == 'Scissors':
         print("You both choose scissors, it's a tie!")
+    return
 
 
 def main():
     "Welciome to the computerized Rock, Paper, Scissors Game!"
-    get_variables()
     userRock()
     userPaper()
     userScissors()
