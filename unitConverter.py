c9b0d1294024641b1e21ec7bdd77e9a89fef0446
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-08 11:35:53 -0700

    Finished unitConverter

--- unitConverter.py
+++ unitConverter.py
@@ -5,56 +5,74 @@ m = 1
 km = m * 1000
 yd = float(0.9144)
 inches = float(0.0254)
+converted = dict()
 
-def userToMeters():
-    userDistance = input("What distance do you want to convert to meters? Enter your number followed by unit abberviation - 'km', 'mi', 'ft', 'yd', 'in' (eg. '145 ft'): ").split(" ")
-    userValue = int(userDistance[0])
-    userUnit = userDistance[1]
+#convert the og value (userValue) to meters. Use the unit given to reference global variables.
+def conversionToMeters(userUnit, userValue):
+    if userUnit == 'ft':
+        toMeters = ft * userValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    elif userUnit == 'km':
+        toMeters = km * userValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    elif userUnit == 'mi':
+        toMeters = mi * userValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    elif userUnit == 'yd':
+        toMeters = yd * userValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    elif userUnit == 'in':
+        toMeters = inches * userValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    elif userUnit == 'm':
+        toMeters == UserValue
+        converted['firstConverion'] = toMeters
+        print(converted)
+    else:
+        print('invalid user input')
+        return
 
-def userToUnits():
-    userDistance = input("What distance do you want to convert? Enter your number followed by unit abberviation - 'km', 'mi', 'ft', 'yd', 'in' (eg. '145 ft'): ").split(" ")
+#convert from meters to whatever value indicated by user in userConvertedUnit
+def conversiontoUnits():
+    if 'ft' in converted:
+        result = ft * converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+    if 'km' in converted:
+        result = km * converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+    if 'mi' in converted:
+        result = mi * converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+    if 'yd' in converted:
+        result = yd * converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+    if 'in' in converted:
+        result = inches * converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+    if 'm' in converted:
+        result = converted['firstConverion']
+        converted['finalconversion'] = result
+        print(converted)
+
+def main():
+    userDistance = input("What distance do you want to convert? Enter your number followed by unit abberviation - 'km', 'm' 'mi', 'ft', 'yd', 'in' (eg. '145 ft'): ").split(" ")
     userValue = int(userDistance[0])
     userUnit = userDistance[1]
     userConvertedUnit = input("What unit do you want to convert to? ")
 
-def converstionToMeters():
-    userToMeters()
-    if userUnit == 'ft':
-        toMeters = ft * userValue
-        #print(f'{userValue} {userUnit} is {ft * userValue} meters')
+    converted[userConvertedUnit] = None
+    conversionToMeters(userUnit, userValue)
+    conversiontoUnits()
+    print(f"{userValue} {userUnit} is {converted['finalconversion']} {userConvertedUnit}")
 
-    elif userUnit == 'km':
-        toMeters = km * userValue
-        #print(f'{userValue} {userUnit} is {km * userValue} meters')
-
-    elif userUnit == 'mi':
-        toMeters = mi * userValue
-        #print(f'{userValue} {userUnit} is {mi * userValue} meters')
-
-    elif userUnit == 'yd':
-        toMeters = yd * userValue
-        #print(f'{userValue} {userUnit} is {yd * userValue} meters')
-    elif userUnit == 'in':
-        toMeters = inches * userValue
-        #print(f'{userValue} {userUnit} is {inches * userValue} meters')
-
-def conversionToUnits():
-    userToUnits()
-    converstionToMeters()
-    if userConvertedUnit == 'ft':
-        result = ft * toMeters
-        print(result)
-    elif userConvertedUnit == 'km':
-        result = km * toMeters
-        print(result)
-    elif userConvertedUnit == 'mi':
-        result = mi * toMeters
-        print(result)
-    elif userConvertedUnit == 'yd':
-        result = yd * toMeters
-        print(result)
-    elif userConvertedUnit == 'in':
-        result = inches * toMeters
-        print(result)
-
-conversionToUnits()
+main()
