8d0f03be3113286323ab2bacde12b1b7114da474
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-09 10:34:29 -0700

    finished simpleCalculator

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,22 +1,44 @@
 
-
-
-def plus(firstN, secondN):
-    firstN + secondN
-def minus(firstN, secondN):
-    firstN - secondN
-def multiply(firstN, secondN):
-    firstN * secondN
-def divide(firstN, secondN):
-    firstN / secondN
-
-simpleCalc = {
-    '+':plus()
-    '-':minus()
-    '*':multiply()
-    '/':divide()
-}
-def getUserInput():
+def main():
     userOperator = input('What operation would you like to perform? ')
-    firstN = float(input('What is your first number?'))
-    secondN = float(input('What is your second number?')
+    firstN = float(input('What is your first number? '))
+    secondN = float(input('What is your second number? '))
+
+    def plus(x, y):
+        result = firstN + secondN
+        if userOperator == '+':
+            print(result)
+    def minus(x, y):
+        result = firstN - secondN
+        if userOperator == '-':
+            print(result)
+    def multiply(x, y):
+        result = firstN * secondN
+        if userOperator == '*':
+            print(result)
+    def divide(x, y):
+        result = firstN / secondN
+        if userOperator == '/':
+            print(result)
+
+    simpleCalc = {
+        '+' : plus(firstN, secondN),
+        '-' : minus(firstN, secondN),
+        '*' : multiply(firstN, secondN),
+        '/' : divide(firstN, secondN),
+    }
+'''
+    def calculate(userOperator):
+        if userOperator == '+':
+            print(simpleCalc['+']())
+
+        elif userOperator == '-':
+            print(simpleCalc['-']())
+
+        elif userOperator == '*':
+            print(simpleCalc['*']())
+
+        elif userOperator == '/':
+            print(simpleCalc['/']())
+'''
+main()
