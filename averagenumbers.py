e4fa243906e08fbd169ca16650a92e2348e5f2ec
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-08 21:22:02 -0700

    fixed bugs

--- averagenumbers.py
+++ averagenumbers.py
@@ -6,9 +6,8 @@ divide final sum by the number of elements in the list
 maybe use the * discussed yesterday in class
 '''
 import string
-
+finalN = []
 def getNumbers():
-    finalN = []
     while True:
         userN = input("Enter numbers you want to averge. When finished, type 'done': ")
         if userN.isdigit():
@@ -16,3 +15,15 @@ def getNumbers():
             print(finalN)
         else:
             break
+def averageNumbers():
+    #make each item, in finalN, a float if that item is a number.
+    conversionToFloat = [float(n) for n in finalN if n]
+    print(conversionToFloat)
+    #ternery operator approach: a if condition else b.
+    #condition is evaluated to be True of False(booleant). If True, only a is evaluated; if False, only b is evaluated. Assign condition to averagedNumbers.
+    #conditional expressions are very useful when in a 'one value or another' situation.
+    averagedNumber = sum(conversionToFloat)/len(conversionToFloat) if conversionToFloat else '-'
+    print(averagedNumber)
+
+getNumbers()
+averageNumbers()
