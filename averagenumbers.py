636409ca0fceda7b51c4602c9ff648ee4e3ef08c
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-08 19:52:07 -0700

    added function

--- averagenumbers.py
+++ averagenumbers.py
@@ -5,3 +5,14 @@ divide final sum by the number of elements in the list
 
 maybe use the * discussed yesterday in class
 '''
+import string
+
+def getNumbers():
+    finalN = []
+    while True:
+        userN = input("Enter numbers you want to averge. When finished, type 'done': ")
+        if userN.isdigit():
+            finalN.append(userN)
+            print(finalN)
+        else:
+            break
