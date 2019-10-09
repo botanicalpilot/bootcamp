e5a4de35ffe58f618516e79268dc83f26f65e3ed
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-08 18:19:26 -0700

    added code to averageNumbers

---
+++ averagenumbers.py
@@ -0,0 +1,7 @@
+'''
+make a list of numbers from user input
+make a running sum of that list of numbers
+divide final sum by the number of elements in the list
+
+maybe use the * discussed yesterday in class
+'''
