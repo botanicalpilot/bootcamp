6ba1c62f11ccb8eab23d0edb651dbad7e0fe69ca
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-10 10:36:08 -0700

    completed cipher, lab13

--- newCipher.py
+++ newCipher.py
@@ -2,31 +2,24 @@ import string
 
 def cipher():
     letters = string.ascii_lowercase
-    userMessage = input("Enter your string you want deciphered: ")
+    userMessage = input("Enter your string you want deciphered: ").lower()
+    #make the shifted letter list for the table below. start at position 13 in letters and go to end of list. Add the begining of the list up to the 13th item. 
     cipherLetters = letters[13:] + letters[:13]
-
-    #print(cipherLetters)
-    #cipherDic = dict(zip(letters, cipherLetters))
-    #print(cipherDic)
-    #def replace(userMessage,cipherDic):
-    #userList = list(userMessage)
+    #make a table with maketrans(charcter, mappedCharacter)
     table = str.maketrans(letters, cipherLetters)
+    #translate the message using the table. 
     print(userMessage.translate(table))
 
-
-   # replacedMessage = []
-    #for l in userList:
-        #replacedMessage.append(cipherDic.get(letters[cipherLetters]))
-   # print(replacedMessage)
+cipher()
 
 
 '''
-I've tried two things without success and now i am at a loss. 
+Things I tried:
 I first tried makeing a dictionary using zip with the offset in cipherLetters. That worked for making the dictionary, but then I couldn't replace the values from each letter into a new list that would display the ciphered text. 
 
-I went back to making a table with maketrans and using that table to translate the userMessage into the ciphered text. I feel like I've read enough of this shit now that I understand how these are supposed to work, but I still can't get it to work with my code. 
+I went back to making a table with maketrans and using that table to translate the userMessage into the ciphered text. I feel like I've read enough of this shit now that I understand how these are supposed to work.
 
 I finally got it to work with the table. The problem was I was using 'return' rather than print. Return does just that, returns the value to the caller. Specifically it breaks out of the function you are in, and if you call that function elsewhere it will return the value. 
 
 '''
-cipher()
+
