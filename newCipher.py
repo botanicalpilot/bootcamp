1efb1cf602c47aa0acb1ff4ded19d7983e35b55a
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-10 00:19:16 -0700

    made dict with key matching ciphered value

--- newCipher.py
+++ newCipher.py
@@ -1,9 +1,9 @@
 import string
 
 def cipher():
-    letters = [string.ascii_letters]
+    letters = string.ascii_letters
     userMessage = input("Enter your string you want deciphered: ")
-    cipherLetters = [letters[13:] + letters[:13]]
+    cipherLetters = letters[13:] + letters[:13]
     print(cipherLetters)
     cipherDic = dict(zip(letters, cipherLetters))
     print(cipherDic)
