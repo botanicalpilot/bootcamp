b75bbd9e5751c56c7860626732cbcc9b9807b4c6
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-11 21:00:48 -0700

    completed version 2

--- numberToPhrase.py
+++ numberToPhrase.py
@@ -13,18 +13,25 @@ def onesConversion(x):
 
 def tensExtract(x):
     tens_digit = x // 10
-    return tens_digit
+    removeHundred = tens_digit%10
+    return removeHundred
 
 def tensConversion(x):
     tenMatch = {1 : 'ONE', 2: 'twenty', 3 : 'thirty', 4: 'fourty', 5 : 'fifty', 6 : 'sixty', 7 : 'seventy', 8 : 'eighty', 9 : 'ninety', 0 : ''}
     if x in tenMatch:
         return tenMatch[x]
 
-def teens(x,y):
-    if x == 'ONE':
-        teensDic = {1 : 'eleven', 2 : 'twelve', 3 : 'thirteen', 4 : 'fourteen', 5 : 'fifteen', 6 : 'sixteen', 7 : 'seventeen', 8 : 'eighteen', 9 : 'nineteen'}
-        if y in teensDic:
-            return teensDic[y]
+def teens(y):
+    teensDic = {1 : 'eleven', 2 : 'twelve', 3 : 'thirteen', 4 : 'fourteen', 5 : 'fifteen', 6 : 'sixteen', 7 : 'seventeen', 8 : 'eighteen', 9 : 'nineteen'}
+    if y in teensDic:
+        return teensDic[y]
+
+def hundreds(x):
+    hundredPlace = x//100
+    hundredMatch = {1 : 'one hundred', 2 : 'two hundred', 3 : 'three hundred', 4 : 'four hundred', 5 : 'five hundred', 6 : 'six hundred', 7 : 'seven hundred', 8 : 'eight hundred', 9 : 'ninehundred', 0 : ''}
+    if hundredPlace in hundredMatch:
+        return hundredMatch[hundredPlace]
+
     
 def main():
     #get number from user wanting to convert
@@ -40,9 +47,14 @@ def main():
     
     #test if for teen N
     if tenWord == 'ONE':
-        print(teens(tenWord, oneDigit))
+        print(hundreds(userN))
+        print(teens(oneDigit))
+        
     else:
-        print(tenWord + ' ' + oneWord)
+        print(hundreds(userN))
+        print(tenWord)
+        print(oneWord)
         
        
 main()
+
