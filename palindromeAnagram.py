764a877382b192492c124405e0d17626ec108028
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-11 21:20:21 -0700

    working on palindrome

---
+++ palindromeAnagram.py
@@ -0,0 +1,9 @@
+def getLists():
+    userWord = input("Enter a word to test for palindrome: ")
+    letters = list(userWord)
+    return letters
+
+def check_palidrome(x):
+
+    
+print(getLists())
