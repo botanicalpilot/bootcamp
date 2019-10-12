bd9500d571e6e36f64112e18cb802b7a8041913e
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-12 12:33:19 -0700

    completed version 2

--- palindromeAnagram.py
+++ palindromeAnagram.py
@@ -12,4 +12,21 @@ def main():
     wordList = getLists()
     print(check_palidrome(wordList))
 
-main()
+#uncomment main, and comment out the print on line 32, to run palidrome version
+#main()
+
+def getAnagramOne():
+    anagramPhraseOne = input("Enter a phrase to test for anagram: ").lower().replace(' ', '', -1)
+    return anagramPhraseOne
+    
+def getAnagramTwo():
+    anagramPhraseTwo = input("Enter a second phrase to test for anagram: ").lower().replace(' ', '', -1)
+    return anagramPhraseTwo
+
+def getAnagramList(AnagramOne, AnagramTwo):
+    anagramLettersOne = list(AnagramOne)
+    anagramLettersTwo = list(AnagramTwo)
+    if sorted(anagramLettersOne) == sorted(anagramLettersTwo):
+        return True
+
+print(getAnagramList(getAnagramOne(), getAnagramTwo()))
