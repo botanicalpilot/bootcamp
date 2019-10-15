50c57e51b705f1be4de016a655dc6e934af2cc29
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-15 10:39:24 -0700

    working version 2, not sure if a double A situation works

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,16 @@
 
 import random
 
+#evaluate for Aces in userCards, and total
+def aceEval(userCards, total):
+    aValue = []
+    if 'A' in userCards and total <= 10:
+        aValue.append(11)
+    else:
+        aValue.append(1)
+    aValue.append(total)
+    return sum(aValue)
+
 def hitBustedBlackjack(total):
     if total == 21:
         print('Blackjack!')
@@ -11,7 +21,7 @@ def hitBustedBlackjack(total):
     else:
         print('busted')
 
-
+#get values from list (userCards)
 def getValues(list, cardDict):
     total = []
     for x in list:
@@ -19,6 +29,7 @@ def getValues(list, cardDict):
             total.append(cardDict[x])
     return sum(total)
 
+#randomly choose three cards for our players. Take the input cards from the main functions. 
 def randomCards(cards):
     choosesCards = []
     for i in range(3):
@@ -28,11 +39,20 @@ def randomCards(cards):
 
 def main():
     cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    cardValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    cardValue = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     cardDict = dict(zip(cards, cardValue))
+    print(cardDict)
     userCards = randomCards(cards)
     print(f'Your cards are {userCards}')
-    total = getValues(userCards, cardDict)
-    hitBustedBlackjack(total)
+    if 'A' in userCards:
+        totalNoA = getValues(userCards, cardDict)
+        totalWA = aceEval(userCards, totalNoA)
+        print(totalWA)
+        hitBustedBlackjack(totalWA)
+
+    else:    
+        total = getValues(userCards, cardDict)
+        print(f'Your total is {total}')
+        hitBustedBlackjack(total)
 
 main()
