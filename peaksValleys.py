f9b67eef3f826fc7666f1c7b4357fa6173ef3732
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-14 17:44:14 -0700

    more work on peaks

--- peaksValleys.py
+++ peaksValleys.py
@@ -1,20 +1,34 @@
 
-data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8]
+
 '''
 We need to look at each item in data and see if the items before and after it are lower/lower (peak) or high/high(valley). return the index number for all instances that occur. Then take the peaks and valleys function to return the index of all the peaks and valleys.
 '''
-def display(data):
+import itertools
+
+def enumData(data):
     #use enumerate to make a new list of tuples, index, with the items of (index,value) pairs
     indexData = list(enumerate(data, 1))
-    print(indexData)
+    return indexData
+
+def displayData(indexData):
     for index, val in indexData:
         star = 'x' * val
         indexValue = index
         print(indexValue, star)
-    
+        print(indexValue, val)
+
+def peak(indexData):
+  
+        
 #def valleys():
 
-
 #def peaksAndValleys():
+def main():
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8]
+    indexData = enumerate(data)
+    displayData(indexData)
 
-print(display(data))
+#main()
+
+
+'how can I compare the items in a list or a truple to those in front and being them? and return the value of their index?'
