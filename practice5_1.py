0007d95cd0606df702e1f4ef21788cc6a2ce412b
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-16 19:24:32 -0700

    actually completed 5-1

--- practice5_1.py
+++ practice5_1.py
@@ -1,5 +1,6 @@
-def powersTwo():
-    numbers = [i ** 2 for i in range(1, 11)]
-    return numbers
-print(powersTwo())
+print((2 ** 9))
 
+def powersTwo(x):
+    y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return[x ** y for y in range(10)]
+print(powersTwo(2))
