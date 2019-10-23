253ab141b1f2a7944dcf544514bec9b48c13ea51
Akins <Akins@RONNIES-MAC-BOOK.local>
2019-10-23 11:14:51 -0700

    finished practice 4_3

--- practice4_3.py
+++ practice4_3.py
@@ -2,16 +2,14 @@ import string
 
 d = {'a1':5, 'a2':2, 'a3':3, 'b1':10, 'b2':1, 'b3':1, 'c1':4, 'c2':5, 'c3':6}
 
-letters = list(string.ascii_lowercase)
+def averageDict(d):
+    a = [v for k, v in d.items() if k.startswith('a')]
+    b = [v for k, v in d.items() if k.startswith('b')]
+    c = [v for k, v in d.items() if k.startswith('c')]
+    newdict = {'a': (sum(a)/len(a)), 'b': (sum(b)/len(b)), 'c': (sum(c)/len(c))}
+    return newdict
 
-for item in letters:
-    for key in d:
-        butt = dict(item)
-        if key.startswith(item, 0 , 2):
-            print(d.viewkeys())
+print(averageDict(d))
 
-
-'''
-need to figure out how to get values for each letter:
-
-'''
+        
+    
